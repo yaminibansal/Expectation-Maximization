3429082a8b6adfc7fa79adbde18a150ef146f91c
Yamini <yamini63bansal@gmail.com>
2016-05-24 11:17:14 +0530

    Revert "Some more experiments"
    This reverts commit ef9af6a8e1cb5a987dc5a9aff3ee2410c85363b8.

--- muPlotter.m
+++ muPlotter.m
@@ -1,12 +1,10 @@
 function[] = muPlotter(mu, pi, noRows, noCols, K, n, r, c )
 %% Assumes mu is [noRows*noCols, K]
-[piTemp, Ind]=sort(pi, 'descend');
-muTemp=(mu(:,Ind));
 
 figure(n)
 for i=1:K
     subplot(r, c, i)
-    imagesc(reshape(muTemp(:, i), noRows, noCols))
-    title(num2str(piTemp(i)))
+    imagesc(reshape(mu(:, i), noRows, noCols))
+    title(num2str(pi(i)))
     colormap gray
 end    
